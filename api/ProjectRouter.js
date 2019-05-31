3d1e0100b4f1343c8d9836abd825c46fb5f45570
amlane <twelvexstring@gmail.com>
2019-05-31 10:36:31 -0500

    post endpoint set up and working, checking for name and description to both have values or it won't post, maybe I'll make a custom contentsCheck middleware later

--- api/ProjectRouter.js
+++ api/ProjectRouter.js
@@ -12,4 +12,20 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    const { name, description } = req.body;
+    if(!name || !description){
+        res.status(400).json({ message: "Project name and description is required." })
+    } else {
+        
+    Projects.insert({ name, description })
+    .then( newProject => {
+        res.status(201).json(newProject)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "There was an error trying to post a new project to the database." })
+    })
+    }
+})
+
 module.exports = router;
