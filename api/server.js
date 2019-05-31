1710bbd8c671e8efa0be71ceac24a4dca28e67c4
amlane <twelvexstring@gmail.com>
2019-05-31 10:23:14 -0500

    added first route to projects and set up a custom middleware logger

--- api/server.js
+++ api/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
+const projectRouter = require('./ProjectRouter.js');
+
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
     res.send("Let's build an API!")
 })
+
+server.use('/api/projects', projectRouter);
+
+function logger(req, res, next){
+    const time = new Date().toISOString();
+    console.log(`A ${req.method} was made to ${req.url} at ${time}`);
+    next();
+}
 
 module.exports = server;
