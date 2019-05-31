71370c30fd997f104c004d40645e0ba0384d0778
amlane <twelvexstring@gmail.com>
2019-05-31 10:27:44 -0500

    Saw the first project from my get request made on postman

--- api/ProjectRouter.js
+++ api/ProjectRouter.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 
 const Projects = require('../data/helpers/projectModel');
 
+router.get('/', (req, res) => {
+    Projects.get()
+    .then( projects => {
+        res.status(200).json(projects)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "There was an error trying to retrieve the projects from the database." })
+    })
+})
+
 module.exports = router;
